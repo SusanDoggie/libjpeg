d230ae424ec4a89ccd546e22aa66515c969bcbe0
Susan Cheng <susan.doggie@gmail.com>
2022-01-23 15:11:27 +0800

    update

--- Sources/include/libjpeg/jconfigint.h
+++ Sources/include/libjpeg/jconfigint.h
@@ -1,5 +1,5 @@
 /* libjpeg-turbo build number */
-#define BUILD  "20211112"
+#define BUILD  "20220123"
 
 /* Compiler's inline keyword */
 #undef inline
@@ -14,7 +14,7 @@
 #define PACKAGE_NAME  "libjpeg-turbo"
 
 /* Version number of package */
-#define VERSION  "2.1.2"
+#define VERSION  "2.1.3"
 
 /* The size of `size_t', as computed by sizeof. */
 #define SIZEOF_SIZE_T  8
